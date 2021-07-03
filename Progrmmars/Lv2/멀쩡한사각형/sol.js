c8a89736170cc055ad0d83b53ae25c6ac7e1b6cb
Chanuk Kim <flew9412@gmail.com>
2021-07-03 20:57:49 +0900

    Update solve

--- Progrmmars/Lv2/멀쩡한사각형/sol.js
+++ Progrmmars/Lv2/멀쩡한사각형/sol.js
@@ -1,4 +1,12 @@
 function solution(w, h) {
+    if(h>w){
+        answer = h;
+        h = w;
+        w = answer;
+        answer = 0;        
+    } 
+    // 소수점에서 오류나는거 같아서
+    // y=x 대칭시켜줌
     var answer = 0;
     let lastH = h;
     let nowH = 0;
