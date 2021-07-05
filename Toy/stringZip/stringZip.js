72e22704897f1a0775a95e3d7b381ae1bdd59eae
Chanuk Kim <flew9412@gmail.com>
2021-07-05 13:15:50 +0900

    중복삭제

--- Toy/stringZip/stringZip.js
+++ Toy/stringZip/stringZip.js
@@ -1,3 +1,18 @@
 function compressString(str) {
-    // TODO: 여기에 코드를 작성합니다.
+    let zipStr = new Array();
+    let i = 0;
+    while(str.length > 0){
+        if(str[i] !== str[i+1]){
+
+            if(str.slice(0,i+1).length >= 3){
+                zipStr.push(str.slice(0,i+1).length + str[0])
+            }else zipStr.push(str.slice(0,i+1));
+
+            str = str.slice(i+1);
+            i = 0;
+            continue;
+        }
+        i++;
+    }
+    return zipStr.join('');
 }
