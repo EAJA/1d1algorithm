41c5b604047793b7256f34989701c5fd1b4643a0
Chanuk Kim <flew9412@gmail.com>
2021-07-13 00:07:22 +0900

    solved updates

--- Progrmmars/Lv2/예상대진표/sol.js
+++ Progrmmars/Lv2/예상대진표/sol.js
@@ -1,20 +1,21 @@
 function solution(n, a, b) {
 
+    let answer = [String(a) + String(b), String(b) + String(a)]
     let match = new Array();
     let matchArr = new Array();
     let now = 1;
 
     for (let i = 0; i < n; i++) {
-        if (i === a - 1) match.push(a)
-        else if (i === b - 1) match.push(b)
+        if (i === a - 1) match.push(String(a))
+        else if (i === b - 1) match.push(String(b))
         else match.push('');
     }
 
-    for(let q = 2; q <= n; q = q**2) {
+    for (let q = 2; q <= n; q *= 2) {
 
         for (let i = 0; i < match.length; i += 2) {
-            matchArr.push(match.slice(i, i + 2).join(''));
-            if (matchArr[i / 2] === a + '' + b || matchArr[i / 2] === b + '' + a) {
+            matchArr.push(match[i] + match[i + 1]);
+            if (matchArr[i / 2] === answer[0] || matchArr[i / 2] === answer[1]) {
                 return now;
             }
         }
@@ -25,6 +26,3 @@ function solution(n, a, b) {
     }
     return now;
 }
-
-console.log(solution(16, 4, 15));
-//3
